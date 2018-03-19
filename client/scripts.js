74da1d73fd226f1178edb30013064f5ffa253a40
pgorovoy <pgorovoy@playflock.com>
2018-03-20 02:36:00 +0300

    flights

--- client/scripts.js
+++ client/scripts.js
@@ -1,133 +1,103 @@
-const domodedovo = ['55.410307', '37.902451'];
+const URL = 'https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=56.84,55.27,33.48,41.48';
+const xhr = new XMLHttpRequest();
 const deg2rad = deg => deg * (Math.PI / 180);
 
-const getDistance = (lat1, lon1, lat2 = domodedovo[0], lon2 = domodedovo[1]) => {
-  const R = 6371;
-  const dLat = deg2rad(lat2 - lat1);
-  const dLon = deg2rad(lon2 - lon1);
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+class Fly {
+  constructor() {
+    this.data = {};
+    this.flights = [];
+    this.feet = 3.28084;
+    this.domodedovo = ['55.410307', '37.902451'];
+  }
+
+  getDistance(lat1, lon1, lat2 = this.domodedovo[0], lon2 = this.domodedovo[1]) {
+    const R = 6371;
+    const dLat = deg2rad(lat2 - lat1);
+    const dLon = deg2rad(lon2 - lon1);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + 
     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return R * c; // km
-};
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c; // km
+  }
 
-const tableHeadersNames = {
-  latitude: 'Широта',
-  longitude: 'Долгота',
-  course: 'Курс(град)',
-  altitude: 'Высота полета(м)',
-  speed: 'Скорость(км/ч)',
-  'departure-code': 'Код вылета',
-  'destination-code': 'Код назначения',
-  flight: 'Номер рейса',
-};
+  clearFlights() {
+    this.flights = [];
+  }
 
-const xhr = new XMLHttpRequest();
-setInterval(() => {
-  xhr.open('GET', 'https://data-live.flightradar24.com/zones/fcgi/feed.js', true);
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState !== 4) {
-      return;
-    }
-    if (xhr.status !== 200) {
-      console.log(`status = ${xhr.status} : text = ${xhr.statusText}`);
-    } else {
-      const data = JSON.parse(xhr.responseText);
-      const root = document.getElementById('root');
-      const table = document.createElement('table');
-      const tableHeader = document.createElement('caption');
-      const tableHeaders = document.createElement('tr');
-      const lastTable = document.getElementById('table');
-      const flights = [];
-
-      if (lastTable) {
-        lastTable.parentNode.removeChild(lastTable);
+  fetchData() {
+    xhr.open('GET', URL, true);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== 4) {
+        return;
       }
-      table.setAttribute('id', 'table');
-      tableHeader.className = 'table__main-header';
-      tableHeader.innerText = 'Данные';
-      tableHeaders.className = 'table__headers';
+      if (xhr.status !== 200) {
+        console.error(`status = ${xhr.status} : text = ${xhr.statusText}`);
+      } else {
+        try {
+          this.data = JSON.parse(xhr.responseText);
+        } catch (e) {
+          console.error('error = ', e);
+        }
+      }
+    };
+    xhr.send();
+  }
 
-      table.appendChild(tableHeader);
-      table.appendChild(tableHeaders);
-      root.appendChild(table);
-      const keys = Object.keys(tableHeadersNames);
-      keys.forEach((key) => {
-        const th = document.createElement('th');
-        th.className = `table__headers table__headers_${key}`;
-        th.innerText = tableHeadersNames[key];
-        tableHeaders.appendChild(th);
-      });
-      const dataKeys = Object.keys(data);
-      dataKeys.forEach((key) => {
-        if (key !== 'full_count' && key !== 'version') {
-          const airData = data[key];
+  render() {
+    const table = document.getElementById('table');
+
+    this.interval = setInterval(() => {
+      this.fetchData();
+
+      const dataKeys = Object.keys(this.data);
+      dataKeys.forEach((id) => {
+        if (id !== 'full_count' && id !== 'version') {
+          const airData = this.data[id];
           const [, latitude, longitude, course, altitude, speed,,,,,,
             departureCode, destionationCode, flight] = airData;
+          const height = (altitude / this.feet).toFixed(0);
           const flightData = {
-            latitude, longitude, course, altitude, speed, departureCode, destionationCode, flight,
+            id,
+            latitude,
+            longitude,
+            course,
+            altitude: height,
+            speed,
+            departureCode,
+            destionationCode,
+            flight,
           };
-          flights.push(flightData);
+          this.flights.push(flightData);
         }
       });
-      flights.sort((a, b) => {
-        const dist1 = getDistance(a.latitude, a.longitude);
-        const dist2 = getDistance(b.latitude, b.longitude);
+      this.flights.sort((a, b) => {
+        const dist1 = this.getDistance(a.latitude, a.longitude);
+        const dist2 = this.getDistance(b.latitude, b.longitude);
         return dist1 - dist2;
       });
-      flights.forEach((flight) => {
+      this.flights.forEach((flight) => {
         const flightRow = document.createElement('tr');
         const fields = Object.keys(flight);
 
         fields.forEach((field) => {
-          const td = document.createElement('td');
-          td.innerText = flight[field];
-          flightRow.appendChild(td);
+          if (field !== 'id') {
+            const td = document.createElement('td');
+            td.innerText = flight[field];
+            flightRow.appendChild(td);
+          }
         });
-        flightRow.className = `flight flight-${flight}`;
-        table.appendChild(flightRow);
+        flightRow.className = `flight flight-${flight.id}`;
+        const lastRow = document.getElementsByClassName(`flight-${flight.id}`)[0];
+        if (lastRow) {
+          lastRow.parentNode.replaceChild(flightRow, lastRow);
+        } else {
+          table.appendChild(flightRow);
+        }
       });
-    }
-  };
-  xhr.send();
-}, 3000);
-
-// for (let key in tableHeadersNames) {
-//   const th = document.createElement('th');
-//     th.className = `table__headers table__headers_${key}`;
-//     th.innerText = tableHeadersNames[key];
-//     tableHeaders.appendChild(th);
-// }
-
-// for (let key in data) {
-//   if (key !== 'full_count' && key !== 'version') {
-//     const airData = data[key];
-//     const [, latitude, longitude, course, altitude, speed,,,,,,
-//         departureCode, destionationCode, flight ] = airData;
-//     const flightData = {
-//         latitude, longitude, course,
-//         altitude, speed, departureCode,
-//         destionationCode, flight
-//     };
-//     flights.push(flightData);
-//   }
-// }
-
-// for (let field in flight) {
-//   const td = document.createElement('td');
-//   td.innerText = flight[field];
-//   flightRow.appendChild(td);
-// }
-
-// for (let flight of flights) {
-//   const flightRow = document.createElement('tr');
-//   for (let field in flight) {
-//     const td = document.createElement('td');
-//     td.innerText = flight[field];
-//     flightRow.appendChild(td);
-//   }
-//   flightRow.className = `flight flight-${flight}`;
-//   table.appendChild(flightRow);
-//   }
-// }
+      this.clearFlights();
+    }, 3000);
+  }
+}
+const fly = new Fly();
+fly.render();
